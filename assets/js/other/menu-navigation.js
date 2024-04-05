ddcb24ed0cc7c40e2d581fb353cea892db0aed0d
Dibbiii <alessandra.dibella@gmail.com>
2024-04-06 00:08:33 +0200

    Co-authored-by: Alessandro Bruno <tacosalfornoh@users.noreply.github.com>

---
+++ assets/js/other/menu-navigation.js
@@ -0,0 +1,11 @@
+
+navButton = document.querySelector('.fa-bars');
+section = document.querySelector('#menu-navigation');
+paragrafo = document.querySelectorAll('li');
+
+
+for(const paragrafo of )
+navButton.addEventListener('click', function() {
+    console.log('click');
+
+);
--- assets/js/other/visual-compiti.js
+++ assets/js/other/visual-compiti.js
@@ -2,32 +2,38 @@ const descrizioni = document.querySelectorAll("#descrizione");
 const articoli = document.querySelectorAll("article");
 const descrizioniTagliate = [];
 const descrizioniOriginali = [];
-var newDescrizione;
 
 for (const paragrafo of descrizioni) {
-  descrizioniOriginali.push(paragrafo.textContent);
+  const currentText = paragrafo.textContent;
+  descrizioniOriginali.push(currentText);
+  
+  if(currentText.length > 80){
+    const slicedText = currentText.slice(0, 80).concat("...");
+    paragrafo.textContent = slicedText;
+    descrizioniTagliate.push(slicedText);
+    continue;
+  } else {
+    descrizioniTagliate.push(currentText);
+  }
 }
 
-for (const paragrafo of descrizioni) {
-  const newDescrizione = paragrafo.textContent.slice(0, 80).concat("...");
-  paragrafo.textContent = newDescrizione;
-}
+for (const articolo of articoli) {
+  articolo.addEventListener("click", () => {
+    const paragrafo = articolo.querySelector("#descrizione"); // Trova il id di quel paragrafo giusto
 
-for (const paragrafo of descrizioni) {
-  descrizioniTagliate.push(paragrafo.textContent);
-}
-
-// Aggiungi event listener agli articoli
-for (const article of articoli) {
-  article.addEventListener("mouseenter", () => {
-    const paragrafo = article.querySelector("#descrizione"); // Trova il id di quel paragrafo giusto
-    indiceParagrafo = descrizioniTagliate.indexOf(paragrafo.textContent);
-    paragrafo.textContent = descrizioniOriginali[indiceParagrafo];
-  });
-
-  article.addEventListener("mouseleave", () => {
-    const paragrafo = article.querySelector("#descrizione");
-    indiceParagrafo = descrizioniOriginali.indexOf(paragrafo.textContent);
-    paragrafo.textContent = descrizioniTagliate[indiceParagrafo];
+    if (
+      paragrafo.textContent ===
+      descrizioniTagliate[descrizioniTagliate.indexOf(paragrafo.textContent)]
+    ) {
+      indiceParagrafo = descrizioniTagliate.indexOf(paragrafo.textContent);
+      paragrafo.textContent = descrizioniOriginali[indiceParagrafo];
+      articolo.setAttribute("tagliato");
+    } else {
+      const indiceParagrafo = descrizioniOriginali.indexOf(
+        paragrafo.textContent
+      );
+      paragrafo.textContent = descrizioniTagliate[indiceParagrafo];
+      articolo.removeAttribute("tagliato");
+    }
   });
 }
