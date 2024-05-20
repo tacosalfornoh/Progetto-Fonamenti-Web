f65d6083581a59cadc0ed1678a67647cb8fce465
Dibbiii <alessandra.dibella@gmail.com>
2024-05-20 11:12:28 +0200

    a

--- index.js
+++
@@ -1,9 +0,0 @@
-import approfondimento from './page1.js'
-import tabella from './page2.js'
-import modificadati from './modifica_dati.js'
-
-export {
-    tabella,
-    modificadati,
-    approfondimento
-}
